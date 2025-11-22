f9ca654bf9f1e2f8a089cd2775af9e87a061f416
MortiZ <nishitchaudhary71@gmail.com>
2025-11-21 21:18:31 -0500

    add env var in render for cors issue mitigation

--- src/app/Labs/Lab5/client.js
+++ src/app/Labs/Lab5/client.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
 const HTTP_SERVER =
   process.env.NEXT_PUBLIC_HTTP_SERVER ||
   "https://kambaz-node-server-app-z7fi.onrender.com";
@@ -7,46 +9,55 @@ const ASSIGNMENT_API = `${HTTP_SERVER}/lab5/assignment`;
 const TODOS_API = `${HTTP_SERVER}/lab5/todos`;
 
 export const fetchWelcomeMessage = async () => {
-  const response = await axios.get(`${HTTP_SERVER}/lab5/welcome`);
+  const response = await axiosWithCredentials.get(
+    `${HTTP_SERVER}/lab5/welcome`
+  );
   return response.data;
 };
 
 export const fetchAssignment = async () => {
-  const response = await axios.get(`${ASSIGNMENT_API}`);
+  const response = await axiosWithCredentials.get(`${ASSIGNMENT_API}`);
   return response.data;
 };
 
 export const updateTitle = async (title) => {
-  const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
+  const response = await axiosWithCredentials.get(
+    `${ASSIGNMENT_API}/title/${title}`
+  );
   return response.data;
 };
 
 export const fetchTodos = async () => {
-  const response = await axios.get(TODOS_API);
+  const response = await axiosWithCredentials.get(TODOS_API);
   return response.data;
 };
 
 export const removeTodo = async (todo) => {
-  const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
+  const response = await axiosWithCredentials.get(
+    `${TODOS_API}/${todo.id}/delete`
+  );
   return response.data;
 };
 
 export const deleteTodo = async (todo) => {
-  const response = await axios.delete(`${TODOS_API}/${todo.id}`);
+  const response = await axiosWithCredentials.delete(`${TODOS_API}/${todo.id}`);
   return response.data;
 };
 
 export const createNewTodo = async () => {
-  const response = await axios.get(`${TODOS_API}/create`);
+  const response = await axiosWithCredentials.get(`${TODOS_API}/create`);
   return response.data;
 };
 
 export const postNewTodo = async (todo) => {
-  const response = await axios.post(`${TODOS_API}`, todo);
+  const response = await axiosWithCredentials.post(`${TODOS_API}`, todo);
   return response.data;
 };
 
 export const updateTodo = async (todo) => {
-  const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
+  const response = await axiosWithCredentials.put(
+    `${TODOS_API}/${todo.id}`,
+    todo
+  );
   return response.data;
 };
